cc5d84c601ce180c797f7e66af129f560d8290b2
hunglethai <67225023+hunglethai@users.noreply.github.com>
2024-05-02 10:47:55 +0200

    Add files via upload

--- bkz_vary.py
+++ bkz_vary.py
@@ -2,6 +2,7 @@ from fpylll import IntegerMatrix, GSO
 from g6k import *
 from g6k.algorithms.bkz import *
 from fpylll.tools.bkz_stats import dummy_tracer
+from g6k.utils.stats import SieveTreeTracer, dummy_tracer
 from fpylll.tools.quality import basis_quality
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,7 +13,7 @@ fig, axs = plt.subplots(2, 2, figsize=(5, 5))  # Adjust size as needed
 
 
 # Define the dimension of the lattice and the bit size for the integer matrix
-dimension = 100
+dimension = 80
 bits = 8
 
 # Create a random matrix as a basis for the lattice
@@ -48,6 +49,12 @@ trials = 1  # Number of trials or tours to run
 # Running the naive BKZ tour
 for _ in range(trials):
     naive_bkz_tour(g6k, dummy_tracer, blocksize=block_size_bkz, extra_dim4free=extra_dim4free)
+
+tracer = SieveTreeTracer(g6k, root_label=("bkz", dimension), start_clocks=True)
+stat = tracer.trace
+tracer.exit()
+stat = tracer.trace
+print(stat)
 M = GSO.Mat(A)
 M.update_gso()
 
@@ -60,38 +67,40 @@ axs[0, 1].bar(range(1, dimension + 1), norms, color='teal')
 axs[0, 1].set_xlabel('Vector Index')
 axs[0, 1].set_ylabel('Log Norm of naive BKZ Vectors')
 quality = basis_quality(M)
-quality_str = ', '.join(f"{key}: {value:.4f}" for key, value in quality.items())
+quality_str = ', '.join(f"{key}: {value:.6f}" for key, value in quality.items())
 axs[0, 1].set_title('Log Norms of Consecutive naive BKZ Vectors block size of '+str(block_size_bkz)+f'\nBasis Quality: {quality_str}' )
 axs[0, 1].grid(True)
 
 # BKZ PNJ FOR DIFFERENT JUMP
 # BKZ parameters for pump and jump
-block_size = 20
+block_sizes = range(20,dimension,5)
 extra_dim4free = 0
 jumps = range(5,dimension,2) # Test jump = 5 up to the dimension
 trials = 1
 quality_results = []
 
 # Run BKZ pump and jump for different jump values
-for idx, jump in enumerate(jumps):
-    A = A_copy
-    g6k = Siever(A)
+for idx, block_size in enumerate(block_sizes):
+    for jump in jumps:
+        A = A_copy
+        g6k = Siever(A)
+        # print(A==A_copy)
 
-    for _ in range(trials):
-        pump_n_jump_bkz_tour(g6k, dummy_tracer,blocksize=block_size, jump = jump, dim4free_fun= default_dim4free_fun(block_size), extra_dim4free=0,
-                         pump_params=None, goal_r0=0., verbose=False)
+        for _ in range(trials):
+            pump_n_jump_bkz_tour(g6k, dummy_tracer,blocksize=block_size, jump = jump, dim4free_fun= default_dim4free_fun(block_size), extra_dim4free=0,
+                            pump_params=None, goal_r0=0., verbose=False)
 
-    M = GSO.Mat(A)
-    M.update_gso()
-    quality = basis_quality(M)
+        M = GSO.Mat(A)
+        M.update_gso()
+        quality = basis_quality(M)
 
-    # Store quality results
-    quality_results.append((jump, quality))
+        # Store quality results
+        quality_results.append((block_size, jump, quality))
     
 # Print quality results as a table
 df = pd.DataFrame([
-    {'Jump': jump, **quality}
-    for jump, quality in quality_results
+    {'Block size': block_size, 'Jump': jump, **quality}
+    for block_size, jump, quality in quality_results
 ])
 
 # Display the DataFrame
@@ -102,7 +111,7 @@ print(df.to_string(index=False))
 A = A_copy
 g6k = Siever(A)
 for _ in range(trials):
-    pump_n_jump_bkz_tour(g6k, dummy_tracer,blocksize=block_size, jump = jumps[0], dim4free_fun= default_dim4free_fun(block_size), extra_dim4free=0,
+    pump_n_jump_bkz_tour(g6k, dummy_tracer,blocksize=block_sizes[0], jump = jumps[0], dim4free_fun= default_dim4free_fun(block_size), extra_dim4free=0,
                          pump_params=None, goal_r0=0., verbose=False)
 M = GSO.Mat(A)
 M.update_gso()
@@ -117,7 +126,7 @@ axs[1, 1].set_xlabel('Vector Index')
 axs[1, 1].set_ylabel('Log Norm of BKZpnj Vectors')
 quality = basis_quality(M)
 quality_str = ', '.join(f"{key}: {value:.4f}" for key, value in quality.items())
-axs[1, 1].set_title('Log Norms of Consecutive BKZpnj Vectors block size of '+str(block_size) + ' and jump = ' +str(jumps[0])+f'\nBasis Quality: {quality_str}' )
+axs[1, 1].set_title('Log Norms of Consecutive BKZpnj Vectors block size of '+str(block_sizes[0]) + ' and jump = ' +str(jumps[0])+f'\nBasis Quality: {quality_str}' )
 axs[1, 1].grid(True)
 
 # Leave subplot 4 empty or add another plot/info
