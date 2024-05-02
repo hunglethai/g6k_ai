1372e4fd7087380dfb2aeb009e9ec00f9967f788
hunglethai <67225023+hunglethai@users.noreply.github.com>
2024-05-02 09:49:26 +0200

    Update bkz_vary.py

--- bkz_vary.py
+++ bkz_vary.py
@@ -64,14 +64,7 @@ quality_str = ', '.join(f"{key}: {value:.4f}" for key, value in quality.items())
 axs[0, 1].set_title('Log Norms of Consecutive naive BKZ Vectors block size of '+str(block_size_bkz)+f'\nBasis Quality: {quality_str}' )
 axs[0, 1].grid(True)
 
-# BKZ pump and jump
-A =  A_copy
-M = GSO.Mat(A)
-M.update_gso()
-
-# Initialize the Siever
-g6k = Siever(A)
-
+# BKZ PNJ FOR DIFFERENT JUMP
 # BKZ parameters for pump and jump
 block_size = 20
 extra_dim4free = 0
@@ -79,18 +72,16 @@ jumps = range(5,dimension,2) # Test jump = 5 up to the dimension
 trials = 1
 quality_results = []
 
-# BKZ PNJ FOR DIFFERENT JUMP
 # Run BKZ pump and jump for different jump values
 for idx, jump in enumerate(jumps):
     A = A_copy
-    M = GSO.Mat(A)
-    M.update_gso()
     g6k = Siever(A)
 
     for _ in range(trials):
         pump_n_jump_bkz_tour(g6k, dummy_tracer,blocksize=block_size, jump = jump, dim4free_fun= default_dim4free_fun(block_size), extra_dim4free=0,
                          pump_params=None, goal_r0=0., verbose=False)
 
+    M = GSO.Mat(A)
     M.update_gso()
     quality = basis_quality(M)
 
